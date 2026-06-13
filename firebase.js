6bec36a70b1187a6c6bb2802c7dee5f5142958b9
Dan Bij <bijdan95@gmail.com>
2026-06-13 14:43:12 +0300

    Firebase setup

--- firebase.js
+++ firebase.js
@@ -1,21 +1,15 @@
-// Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import {
-  getAuth
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-
-import {
-  getFirestore
-} from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "TA_CLE",
-  authDomain: "TON_AUTH_DOMAIN",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_STORAGE_BUCKET",
-  messagingSenderId: "TON_SENDER_ID",
-  appId: "TON_APP_ID",
-  measurementId: "TON_MEASUREMENT"
+  apiKey: "AIzaSyAF2d0MA7FE_D3M85mbVWqGNLUFhxh_GRw",
+  authDomain: "kazi-app-2d673.firebaseapp.com",
+  projectId: "kazi-app-2d673",
+  storageBucket: "kazi-app-2d673.firebasestorage.app",
+  messagingSenderId: "15005462545",
+  appId: "1:15005462545:web:70996ece8b86937fc681b0",
+  measurementId: "G-CPQVDNR43Y"
 };
 
 const app = initializeApp(firebaseConfig);
